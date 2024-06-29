a001983ce3cc7d681d11bc0d78735ecdc7d10585
Arezoo <arezooayoblo@gmail.com>
2024-06-29 13:00:05 +0330

    navbar

---
+++ front/src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import styled from "./navbar.module.css"
+
+function Navbar(){
+    return(
+        <div className={styled.header}>
+          <ul>
+            <li>لیست مقالات </li>
+            <li> مقاله جدید</li>
+            <li> درباره </li>
+          </ul>
+        </div>
+    )
+}
+
+export default Navbar;
